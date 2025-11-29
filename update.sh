26ba9ce9e186ded3fe4ef824ace6002ec19040f7
kanameg <kaname.g@gmail.com>
2025-11-29 23:12:11 +0900

    update.shを更新番組追加

--- update.sh
+++ update.sh
@@ -5,3 +5,4 @@ set -euo pipefail
 ./run_pipeline.sh 83RW6PK3GG    # 中学生の基礎英語 レベル２
 ./run_pipeline.sh 148W8XX226    # 中学生の基礎英語 レベル1
 ./run_pipeline.sh 8Z6XJ6J415    # 英会話タイムトライアル
+# ./run_pipeline.sh GLZQ4M519X    # Asian View
