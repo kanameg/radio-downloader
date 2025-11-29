c18a619db6d455c1c39fee0f225ab9550881e115
kanameg <kaname.g@gmail.com>
2025-11-29 23:05:10 +0900

    update.shを追加

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+./run_pipeline.sh BR8Z3NX7XM    # エンジョイ・シンプル・イングリッシュ
+./run_pipeline.sh 83RW6PK3GG    # 中学生の基礎英語 レベル２
+./run_pipeline.sh 148W8XX226    # 中学生の基礎英語 レベル1
+./run_pipeline.sh 8Z6XJ6J415    # 英会話タイムトライアル
